cc0f3e4ea5c77d1d0d905bb29aba32e493fb4307
Jeremy Adams <3187222+jpadams@users.noreply.github.com>
2020-02-05 18:51:08 -0800

    Update eks-fargate-setup.sh (#1)
    * Update eks-fargate-setup.sh to use fine-grained IAM for ALB

--- scripts/eks-fargate-setup.sh
+++ scripts/eks-fargate-setup.sh
@@ -2,14 +2,12 @@
 
 CLUSTER_NAME=<your desired EKS cluster name>
 AWS_REGION=<your region that supports EKS/Fargate, e.g. eu-west-1>
-#assumes your AWS creds are in ~/.aws/credentials
-AWS_ACCESS_KEY_ID=$(awk '-F=' '/aws_access_key_id/ { print $2 }' ~/.aws/credentials)
-AWS_SECRET_ACCESS_KEY=$(awk '-F=' '/aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
 
 #check for needed commands
 command -v eksctl >/dev/null 2>&1 || { echo >&2 "I require eksctl but it's not installed.  Aborting."; exit 1; }
 command -v aws >/dev/null 2>&1 || { echo >&2 "I require the aws cli but it's not installed.  Aborting."; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed.  Aborting."; exit 1; }
+command -v sed >/dev/null 2>&1 || { echo >&2 "I require sed but it's not installed.  Aborting."; exit 1; }
 command -v kubectl >/dev/null 2>&1 || { echo >&2 "I require kubectl but it's not installed.  Aborting."; exit 1; }
 
 #https://docs.aws.amazon.com/eks/latest/userguide/pod-execution-role.html
@@ -34,52 +32,36 @@ aws iam attach-role-policy --role-name AmazonEKSFargatePodExecutionRole --policy
 #create the EKS Fargate cluster
 eksctl create cluster --name $CLUSTER_NAME --version 1.14 --fargate
 
+#https://aws.amazon.com/blogs/opensource/kubernetes-ingress-aws-alb-ingress-controller/
 #https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
 eksctl utils associate-iam-oidc-provider \
-               --name $CLUSTER_NAME \
+               --cluster $CLUSTER_NAME \
                --approve
+               
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
 
-
-#https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html
-#Confirming that correct 'elb' tags in place. These look good by default.
-#aws eks describe-cluster --name $CLUSTER_NAME
-#aws eks describe-cluster --name $CLUSTER_NAME | jq '.cluster.resourcesVpcConfig.subnetIds[]' | xargs aws ec2 describe-subnets --subnet-ids 
-#aws eks describe-cluster --name $CLUSTER_NAME | jq '.cluster.resourcesVpcConfig.subnetIds[]' | xargs aws ec2 describe-subnets --subnet-ids | grep elb -B2 -A1
-
-#steps for ALB for ingress since NLB/CLB not yet supported for EKS Fargate
-#https://github.com/aws/containers-roadmap/issues/617
-AWS_VPC_ID=$(aws eks describe-cluster --name $CLUSTER_NAME | jq -r '.cluster.resourcesVpcConfig.vpcId')
-echo "AWS VPC ID: $AWS_VPC_ID"
-
-curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json
+curl -sO https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json
 
 POLICY_EXISTING=$(aws iam list-policies | jq -r '.[][] | select(.PolicyName=="ALBIngressControllerIAMPolicy") | .Arn')
-
 if [ $POLICY_EXISTING ]
 then
 POLICY_ARN=$POLICY_EXISTING;
 else
 POLICY_ARN=$(aws iam create-policy --policy-name ALBIngressControllerIAMPolicy --policy-document file://iam-policy.json | jq -r '.Policy.Arn')
 fi
-echo "POLICY ARN: $POLICY_ARN"
 
-ROLE_NAME=$(kubectl -n kube-system describe configmap aws-auth | grep rolearn | cut -d'/' -f2)
-echo "ROLE NAME: $ROLE_NAME"
+eksctl create iamserviceaccount \
+       --cluster=$CLUSTER_NAME \
+       --namespace=kube-system \
+       --name=alb-ingress-controller \
+       --attach-policy-arn=$POLICY_ARN \
+       --override-existing-serviceaccounts \
+       --approve
 
-aws iam attach-role-policy \
---policy-arn $POLICY_ARN \
---role-name $ROLE_NAME 
+AWS_VPC_ID=$(aws eks describe-cluster --name $CLUSTER_NAME | jq -r '.cluster.resourcesVpcConfig.vpcId')
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml
-
-kubectl get deployment.apps/alb-ingress-controller -n kube-system -o json | jq '.spec.template.spec.containers[0].args += ["--cluster-name='$CLUSTER_NAME'", "--aws-vpc-id='$AWS_VPC_ID'", "--aws-region='$AWS_REGION'"]' | kubectl apply -f -
-
-#I should be using Fine Grained IAM roles, but this AWS ACCESS KEY approach is much simpler to implement for me at the moment.
-kubectl get deployment.apps/alb-ingress-controller -n kube-system -o json | jq '.spec.template.spec.containers[0].env += [ { "name": "AWS_ACCESS_KEY_ID", "value": "'$AWS_ACCESS_KEY_ID'" }, { "name": "AWS_SECRET_ACCESS_KEY", "value": "'$AWS_SECRET_ACCESS_KEY'" } ]' | kubectl apply -f -
-#https://aws.amazon.com/blogs/opensource/introducing-fine-grained-iam-roles-service-accounts/
-#https://github.com/kubernetes-sigs/aws-alb-ingress-controller/issues/1092#issuecomment-561400256
-#https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
-#https://docs.aws.amazon.com/eks/latest/userguide/create-service-account-iam-policy-and-role.html
-#https://docs.aws.amazon.com/eks/latest/userguide/specify-service-account-role.html
+curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
+     | sed "s/# - --cluster-name=devCluster/- --cluster-name=$CLUSTER_NAME/g" \
+     | sed "s/# - --aws-region=us-west-1/- --aws-region=$AWS_REGION/g" \
+     | sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$AWS_VPC_ID/g" \
+     | kubectl apply -f -
